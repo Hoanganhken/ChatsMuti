53e5d6d01749bb787df1a919d021335724ac2106
Hoanganhken <hoanganhken98@gmail.com>
2017-11-22 19:44:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/munichats/chatsmuti/Splash.java
@@ -0,0 +1,13 @@
+package com.example.munichats.chatsmuti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
